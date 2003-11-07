5ab03a4cfa6149fa47b8b7ff3e26c280310c8011
Frederic Gobry <gobry@pybliographer.org>
2003-11-07 13:19:57 +0100

    core -> store
    Now, Pyblio.Core is Pyblio.Store.
    
    imported from: gobry@pybliographer.org--public/pybliographer--devel--1.3--patch-12

--- tests/ut_store.py
+++ tests/ut_store.py
@@ -1,16 +1,16 @@
 import os, pybut, sys
 
-from Pyblio import Core, Schema, Attribute
+from Pyblio import Store, Schema, Attribute
 
-class TestCore (pybut.TestCase):
+class TestStore (pybut.TestCase):
 
-    """ Perform tests on the Pyblio.Core module """
+    """ Perform tests on the Pyblio.Store module """
 
     def testEmpty (self):
         """ Create an empty database with a schema """
         
-        schema = Schema.open ('ut_core/s:simple.xml')
-        db = Core.Database (schema)
+        schema = Schema.open ('ut_store/s:simple.xml')
+        db = Store.Database (schema)
 
         assert len (db) == 0
 
@@ -18,7 +18,7 @@ class TestCore (pybut.TestCase):
         db.xmlwrite (file)
         file.close ()
 
-        pybut.fileeq (',,t1.xml', 'ut_core/empty.xml')
+        pybut.fileeq (',,t1.xml', 'ut_store/empty.xml')
 
         os.unlink (',,t1.xml')
         return
@@ -26,23 +26,23 @@ class TestCore (pybut.TestCase):
     def testReadEmpty (self):
         """ A schema in a database is equivalent to outside the database """
         
-        db = Core.open ('ut_core/empty.xml')
+        db = Store.open ('ut_store/empty.xml')
 
         file = open (',,t2.xml', 'w')
         db.schema.xmlwrite (file)
         file.close ()
 
-        pybut.fileeq (',,t2.xml', 'ut_core/s:simple.xml')
+        pybut.fileeq (',,t2.xml', 'ut_store/s:simple.xml')
         
         os.unlink (',,t2.xml')
         return
 
     def testWrite (self):
 
-        schema = Schema.open ('ut_core/s:full.xml')
-        db = Core.Database (schema)
+        schema = Schema.open ('ut_store/s:full.xml')
+        db = Store.Database (schema)
 
-        e = Core.Entry (Core.Key ('entry 1'),
+        e = Store.Entry (Store.Key ('entry 1'),
                         schema.documents ['sample'])
 
         e ['author'] = [ Attribute.Person (last = 'Last 1'),
@@ -50,7 +50,7 @@ class TestCore (pybut.TestCase):
         e ['url']    = [ Attribute.URL ('http://pybliographer.org') ]
         e ['text']   = [ Attribute.Text (u'sample text') ]
         e ['date']   = [ Attribute.Date (year = 2003) ]
-        e ['ref']    = [ Attribute.Reference (Core.Key ('ref')) ]
+        e ['ref']    = [ Attribute.Reference (Store.Key ('ref')) ]
         
         db [e.key] = e
 
@@ -58,8 +58,23 @@ class TestCore (pybut.TestCase):
         db.xmlwrite (fd)
         fd.close ()
 
-        pybut.fileeq (',,t3.xml', 'ut_core/simple.xml')
+        pybut.fileeq (',,t3.xml', 'ut_store/simple.xml')
+
+        os.unlink (',,t3.xml')
         return
-    
+
+    def testRead (self):
+
+        db = Store.open ('ut_store/simple.xml')
         
-pybut.run (pybut.makeSuite (TestCore, 'test'))
+        fd = open (',,t4.xml', 'w')
+        db.xmlwrite (fd)
+        fd.close ()
+
+        pybut.fileeq (',,t4.xml', 'ut_store/simple.xml')
+
+        os.unlink (',,t4.xml')
+        return
+
+    
+pybut.run (pybut.makeSuite (TestStore, 'test'))
