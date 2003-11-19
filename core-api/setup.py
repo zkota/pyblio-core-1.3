a3dedd8433a252801e58da30a07cf681ec377f7f
Frederic Gobry <gobry@pybliographer.org>
2003-11-19 16:42:57 +0100

    make the core api an installable package
    imported from: gobry@pybliographer.org--public/pybliographer--devel--1.3--patch-31

---
+++ core-api/setup.py
@@ -0,0 +1,25 @@
+import os, stat, sys
+
+from distutils.core import setup
+
+version = "0.1"
+
+setup (name = "pybliographer-core",
+       version = version,
+
+       description = "Pybliographer Core API",
+       author = "Frederic Gobry",
+       author_email = 'gobry@pybliographer.org',
+       url = 'http://pybliographer.org/',
+
+       license = 'GPL',
+       
+       long_description = \
+'''
+This module contains the core API of Pybliographer.
+''',
+
+       packages = [ 'Pyblio',
+                    'Pyblio.Importers',
+                    'Pyblio.Stores' ]
+       )
