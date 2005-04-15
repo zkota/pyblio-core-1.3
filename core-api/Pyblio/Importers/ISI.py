65ec798b2cda591b7cd11169ba4c45dfc447bed4
Frederic Gobry <frederic.gobry@epfl.ch>
2005-04-15 14:22:57 +0100

    re-added the ISI parser (naked version)
    This parser handles files exported from the Web of Science.
    
    imported from: gobry@pybliographer.org--public/pybliographer--devel--1.3--patch-135

---
+++ core-api/Pyblio/Importers/ISI.py
@@ -0,0 +1,91 @@
+from Pyblio.Importers import Tagged
+from Pyblio import Attribute
+
+from gettext import gettext as _
+
+import re, string
+
+start_re = re.compile (r'^(\w\w)\s(.*?)\r?$')
+contd_re = re.compile (r'^\s{3,3}(.*?)\r?$')
+
+class ISIParser (Tagged.Parser):
+
+    """ This parser knows how to split RIS records in fields """
+
+    def line_handler (self, line, count):
+
+        if line.strip () == '': return
+
+        m = start_re.match (line)
+
+        if m:
+            tag, data = m.groups ((1, 2))
+
+            if tag in ('FN', 'VR', 'EF'):
+                return
+            
+            if tag == 'PT':
+                self.record_start ()
+
+            elif self.state == self.ST_IN_FIELD:
+                self.field_end ()
+                
+            if tag == 'ER':
+                self.record_end ()
+                return
+            
+            self.field_start (tag, count)
+            self.field_data (data)
+            return
+
+        m = contd_re.match (line)
+        if m:
+            self.field_data ('\n' + m.group (1))
+            return
+
+        if line == 'EF': return
+        
+        raise SyntaxError (_('line %d: unexpected data: %s') % (count, repr (line)))
+
+
+def _mkperson (txt):
+
+    last, first = map (string.strip, txt.split (','))
+    
+    return Attribute.Person (last = last, first = first)
+
+class Importer (Tagged.Importer):
+
+    """ The importer knows how to map the RIS fields to the 'standard'
+    pyblio model."""
+
+    Parser = ISIParser
+
+    mapping = {}
+
+    
+    def person_add (self, field, value):
+
+        ''' Parse a person name in ISI format '''
+
+        self.record [field] = [ _mkperson (txt) for txt in value.split ('\n') ]
+        return
+
+
+    def do_default (self, line, tag, data):
+
+        try:
+            meth, field = self.mapping [tag]
+
+        except KeyError:
+
+            raise SyntaxError (_("line %s: unknown tag '%s'") % (line, tag))
+
+        except ValueError:
+
+            self.emit ('warning',
+                       (_("line %s: unsupported tag '%s'") % (line, tag)))
+            return
+        
+        meth (self, field, data)
+        return
---
+++ core-api/tests/ut_isi.py
@@ -0,0 +1,56 @@
+# -*- coding: latin-1 -*-
+
+import os, pybut, sys, re
+
+from Pyblio.Importers import ISI, Tagged
+from Pyblio import Store, Schema
+
+class Importer (ISI.Importer):
+
+    def __init__ (self):
+        ISI.Importer.__init__ (self)
+        
+        self.mapping = {
+            'TI': (self.text_add,   'title'),
+            'AU': (self.person_add, 'author'),
+            }
+
+    def do_default (self, line, tag, data):
+
+        try:
+            meth, field = self.mapping [tag]
+        except KeyError:
+            return
+        
+        meth (field, data)
+        return
+
+
+class TestISI (pybut.TestCase):
+
+    def parse (self, file):
+
+        fd = open (file)
+
+        self.fn = pybut.dbname ()
+        s = Schema.Schema ('../Schemas/standard.xml')
+        self.db = Store.get ('file').dbcreate (self.fn, s)
+        
+        self.p = Importer ()
+        self.p.parse (fd, self.db)
+        return
+    
+
+    def testText (self):
+
+        self.parse ('ut_isi/text.isi')
+        self.db.save ()
+
+        pybut.fileeq (self.fn, 'ut_isi/text.xml')
+        return
+    
+
+
+suite = pybut.suite (TestISI)
+
+if __name__ == '__main__':  pybut.run (suite)
