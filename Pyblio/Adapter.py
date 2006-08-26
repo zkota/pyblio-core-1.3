7c6d5764a43f690c8f84700fa28f58b577d7e3be
Frederic Gobry <frederic.gobry@epfl.ch>
2006-08-26 03:41:28 +0100

    preliminary adapter support
    Added an Adapter class, and a convenience OneToOneAdapter, when doing a bijective mapping
    between two schemas.
    
    imported from: gobry@pybliographer.org--2006/pybliographer--devel--1.3--patch-44

---
+++ Pyblio/Adapter.py
@@ -0,0 +1,110 @@
+# This file is part of pybliographer
+# 
+# Copyright (C) 1998-2003 Frederic GOBRY
+# Email : gobry@pybliographer.org
+# 	   
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2 
+# of the License, or (at your option) any later version.
+#   
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details. 
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# 
+
+"""
+Adapt a database schema to another one.
+
+Given a database in, say, PubMed format, it is possible to plug an
+adapter on top of it so that it looks as if the database is in, say,
+BibTeX format instead.
+"""
+
+from gettext import gettext as _
+
+from Pyblio.Store import Database
+
+class Adapter(Database):
+
+    """ This class is a db built on top of another db, which behaves
+    as if it were of a different schema."""
+
+    header = None
+    
+    def __init__(self, base):
+        self.base = base
+        return
+
+    def save(self):
+        return self.base.save()
+
+    def xmlread(self):
+        raise RuntimeError(_("Adapter databases cannot be read from file"))
+    
+    
+
+class OneToOneAdapter(Adapter):
+
+    """ This adapter assumes a one-to-one mapping between the source
+    and the target databases. """
+
+
+    def source2target(self, record):
+        """ Translates a record from the source db to the target db """
+        raise NotImplemented('please override')
+
+    def target2source(self, record):
+        """ Translates a record from the target db to the source db """
+        raise NotImplemented('please override')
+
+    def add(self, record):
+        return self.base.add(self.target2source(record))
+
+    def __setitem__(self, key, record):
+        self.base[key] = self.target2source(record)
+
+    def __getitem__(self, key):
+        return self.source2target(self.base[key])
+
+    def has_key(self, key):
+        return self.base.has_key(key)
+
+    def _entries(self):
+        e = self.base.entries
+        
+        class Looper:
+            def itervalues(s):
+                for v in e.itervalues():
+                    yield self.source2target(v)
+                return
+            
+            def iteritems(s):
+                for k, v in e.iteritems():
+                    yield k, self.source2target(v)
+                return
+
+            def iterkeys(s):
+                return e.iterkeys()
+
+            __iter__ = iterkeys
+            
+        return Looper()
+
+    entries = property(_entries, None)
+    
+    def _txo(self):
+        return self.base.txo
+
+    txo = property(_txo, None)
+    
+    def _rs(self):
+        return self.base.rs
+
+    rs = property(_rs, None)
+    
---
+++ tests/ut_adapter.py
@@ -0,0 +1,78 @@
+import pybut
+
+from Pyblio import Adapter, Store, Schema, Attribute
+
+class A2BAdapter(Adapter.OneToOneAdapter):
+    """ This adapter renames field 'a' into field 'b' """
+
+    schema = Schema.Schema(open('ut_adapter/b.sip'))
+
+    def source2target(self, record):
+        new = Store.Record()
+        new.key = record.key
+
+        if 'a' in record:
+            new['b'] = record['a']
+
+        return new
+
+    def target2source(self, record):
+        new = Store.Record()
+        new.key = record.key
+
+        if 'b' in record:
+            new['a'] = record['b']
+
+        return new
+        
+class TestFromAtoB(pybut.TestCase):
+
+    def setUp(self):
+        sa = Schema.Schema(open('ut_adapter/a.sip'))
+
+        fmt = Store.get('memory')
+        self.dba = fmt.dbcreate(None, sa)
+        self.dbb = A2BAdapter(self.dba)
+        return
+
+    def testAddAndDel(self):
+        """ Add to A, and check in B """
+
+        r = Store.Record()
+        k = r.add('a', 'A sample !', Attribute.Text)
+
+        # Add a record in A...
+        self.dba.add(r)
+
+        # ...and it show up in B (with the proper value)
+        res = [self.dbb[k] for k in self.dbb.entries]
+        self.failUnlessEqual(len(res), 1)
+
+        new = res[0]
+        self.failUnlessEqual(new['b'], r['a'])
+
+        res = list(self.dbb.entries.itervalues())[0]
+        self.failUnlessEqual(res['b'], r['a'])
+
+        k, res = list(self.dbb.entries.iteritems())[0]
+        self.failUnlessEqual(res['b'], r['a'])
+
+        del self.dba[k]
+        self.failUnlessEqual(list(self.dbb.entries.itervalues()), [])
+
+
+    def testXMLWrite(self):
+        r = Store.Record()
+        k = r.add('a', 'A sample !', Attribute.Text)
+        self.dba.add(r)
+
+        tmp = pybut.dbname()
+        fd = open(tmp, 'w')
+        self.dbb.xmlwrite(fd)
+        fd.close()
+
+        pybut.fileeq(tmp, 'ut_adapter/saved-b.bip')
+        
+suite = pybut.suite(TestFromAtoB)
+
+if __name__ == '__main__':  pybut.run (suite)
